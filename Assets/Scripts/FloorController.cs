509a7aaa7cb8635fb3e3a824093541f61ace3da2
Yuta Kosuge <ydt.hran2@gmail.com>
2021-03-08 20:28:12 +0900

    Initial commit

---
+++ Assets/Scripts/FloorController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using TMPro;
+
+public class FloorController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var childObject = transform.GetChild(i).gameObject;
+            if (childObject.CompareTag("Block"))
+            {
+                var block = childObject;
+                var blockNumber = block.transform.Find("Block Number").GetComponent<TextMeshPro>();
+                blockNumber.text = (i + 1).ToString();
+            }
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
