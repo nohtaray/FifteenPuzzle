8ba32a413f6169f78eae2f093341452c074fc576
Yuta Kosuge <ydt.hran2@gmail.com>
2021-03-09 19:53:49 +0900

    ブロックを移動できるようにする

--- Assets/Scripts/BlockController.cs
+++ Assets/Scripts/BlockController.cs
@@ -2,20 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class BlockController : MonoBehaviour
 {
-    public TextMeshProUGUI blockNumberText;
+    private bool isDragging = false;
+    private TextMeshPro blockNumber;
+
+    /// <summary>
+    /// localPosition = position / scaleRatio
+    /// position = localPosition * scaleRatio
+    /// </summary>
+    private float scaleRatio;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        blockNumber = transform.Find("Block Number").GetComponent<TextMeshPro>();
+
+        var num = Vector3.Magnitude(transform.position);
+        var den = Vector3.Magnitude(transform.localPosition);
+        scaleRatio = den != 0 ? num / den : 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnMouseDown()
+    {
+        isDragging = true;
+        // transform.position = new Vector3(transform.position.x, transform.position.y * 5, transform.position.z);
+    }
+
+    private void OnMouseUp()
+    {
+        isDragging = false;
+        // transform.position = new Vector3(transform.position.x, transform.position.y / 5, transform.position.z);
+    }
+
+    private void OnMouseDrag()
+    {
+        //var mp = Input.mousePosition;
+        //float x2 = mp.x;
+        //float y2 = mp.y;
+        //float z2 = mp.z;
+        //Debug.Log(string.Format("x2: {0}, y2: {1}, z2: {2}", x2, y2, z2));
+
+        //var mp2 = transform.localPosition;
+        //Debug.Log(string.Format("pos: x={0}, y={1}, z={2}", mp2.x, mp2.y, mp2.z));
+
+
+        var floor = transform.parent.gameObject.GetComponent<FloorController>();
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Vector3 mousePosition = hit.point;
+            transform.localPosition = new Vector3(mousePosition.x / scaleRatio, transform.position.y, mousePosition.z / scaleRatio);
+        }
     }
 }
--- Assets/Scripts/FloorController.cs
+++ Assets/Scripts/FloorController.cs
@@ -6,26 +6,53 @@ using TMPro;
 
 public class FloorController : MonoBehaviour
 {
+    private List<GameObject> blocks;
+    private List<Vector3> positions;
+
     // Start is called before the first frame update
     void Start()
     {
+        blocks = new List<GameObject>();
+        positions = new List<Vector3>();
+
         for (int i = 0; i < transform.childCount; i++)
         {
             var childObject = transform.GetChild(i).gameObject;
             if (childObject.CompareTag("Block"))
             {
-                var block = childObject;
-                var blockNumber = block.transform.Find("Block Number").GetComponent<TextMeshPro>();
-                blockNumber.text = (i + 1).ToString();
+                blocks.Add(childObject);
             }
         }
 
+        for (int i = 0; i < blocks.Count(); i++)
+        {
+            var block = blocks[i];
+            var blockNumber = block.transform.Find("Block Number").GetComponent<TextMeshPro>();
+            blockNumber.text = (i + 1).ToString();
 
+            positions.Add(new Vector3(block.transform.localPosition.x, block.transform.localPosition.y, block.transform.localPosition.z));
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    private void OnMouseDrag()
+    {
+        //Debug.Log("abc");
+        //var mp = Input.mousePosition;
+        //mp.Normalize();
+        //Debug.Log("1: " + mp);
+        //float x = Input.mousePosition.x;
+        //float y = Input.mousePosition.y;
+        //Debug.Log(string.Format("x: {0}, y: {1}", x, y));
+    }
+
+    public void MoveTo(float x, float z, ref GameObject block)
+    {
+        block.transform.localPosition = new Vector3(x, block.transform.localPosition.y, z);
     }
 }
